254baaacacc96747cd9a8119c1cd5549c96170d3
Meshparts <alexandru.dadalau@meshparts.de>
2025-09-07 00:19:41 +0200

    Update combobox.tcl for Tcl 9 support

--- combobox/combobox.tcl
+++ combobox/combobox.tcl
@@ -1,6 +1,8 @@
-package require Tk
+# https://github.com/jcowgar/misctcl/blob/master/combobox/combobox.tcl
 
-namespace eval ttk::combobox {
+package provide mpcombo 1.0
+
+namespace eval ::ttk::combobox {
   set keyaccumulator ""
 
   # state of CTRL key
@@ -8,137 +10,143 @@ namespace eval ttk::combobox {
 }
 
 # Required to escape a few characters due to the string match used.
-proc ttk::combobox::EscapeKey { key } {
-    switch -- $key {
-        bracketleft  { return {\[} }
-        bracketright { return {\]} }
-        asterisk     { return {\*} }
-        question     { return {\?} }
-        quotedbl     { return {\"} }
-        quoteright   -
-        quoteleft    { return {\'} }
-        default      { return $key }
-    }
+proc ::ttk::combobox::EscapeKey { key } {
+  switch -- $key {
+    bracketleft  { return {\[} }
+    bracketright { return {\]} }
+    asterisk     { return {\*} }
+    question     { return {\?} }
+    quotedbl     { return {\"} }
+    quoteright   -
+    quoteleft    { return {\'} }
+    default      { return $key }
+  }
 }
 
-proc ttk::combobox::PrevNext { W dir } {
-    set cur [$W current]
+proc ::ttk::combobox::PrevNext { W dir } {
+  set cur [$W current]
 
-    switch -- $dir {
-        up {
-            if {$cur <= 0} {
-                return
-            }
+  switch -- $dir {
+    up {
+      if {$cur <= 0} {
+        return
+      }
 
-            incr cur -1
-        }
-        down {
-            incr cur
-
-            if {$cur == [llength [$W cget -values]]} {
-                return
-            }
-        }
+      incr cur -1
     }
+    down {
+      incr cur
 
-    $W current $cur
-    event generate $W <<ComboboxSelected>> -when mark
+      if {$cur == [llength [$W cget -values]]} {
+        return
+      }
+    }
+  }
+
+  $W current $cur
+  event generate $W <<ComboboxSelected>> -when mark
 }
 
-proc ttk::combobox::CompleteEntry { W key } {
+proc ::ttk::combobox::CompleteEntry { W key } {
 
-    # Ignore key input while CTRL key is pressed
-    if {$ttk::combobox::ctrlstat} {
-        return
+  # Ignore key input while CTRL key is pressed
+  if {$::ttk::combobox::ctrlstat} {
+    return
+  }
+
+  if { [string length $key] > 1 && [string tolower $key] != $key } {
+    return
+  }
+
+
+  if { [$W instate readonly] } {
+    set value [EscapeKey $key]
+  } else {
+    set value [string map { {[} {\[} {]} {\]} {?} {\?} {*} {\*} } [$W get]]
+    if { [string equal $value ""] } {
+      return
     }
+  }
 
-    if { [string length $key] > 1 && [string tolower $key] != $key } {
-        return
+  set values [$W cget -values]
+
+  set start 0
+  if { [string match -nocase $value* [$W get]] } {
+    # set start [expr { [$W current] + 1 }]
+    set start [$W current]
+    if {$start==-1} {
+      set start 0
     }
+  }
 
-    if { [$W instate readonly] } {
-        set value [EscapeKey $key]
+  set x [lsearch -nocase -start $start $values $value*]
+  if { $x < 0 } {
+    if { $start > 0} {
+      set x [lsearch -nocase $values $value*]
+
+      if { $x < 0 } {
+        return
+      }
     } else {
-        set value [string map { {[} {\[} {]} {\]} {?} {\?} {*} {\*} } [$W get]]
-        if { [string equal $value ""] } {
-            return
-        }
+      return
     }
+  }
 
-    set values [$W cget -values]
+  set index [$W index insert]
+  $W set [lindex $values $x]
+  $W icursor $index
+  $W selection range insert end
 
-    set start 0
-    if { [string match -nocase $value* [$W get]] } {
-        set start [expr { [$W current] + 1 }]
-    }
-
-    set x [lsearch -nocase -start $start $values $value*]
-    if { $x < 0 } {
-        if { $start > 0} {
-            set x [lsearch -nocase $values $value*]
-
-            if { $x < 0 } {
-                return
-            }
-        } else {
-            return
-        }
-    }
-
-    set index [$W index insert]
-    $W set [lindex $values $x]
-    $W icursor $index
-    $W selection range insert end
-
-    if { [$W instate readonly] } {
-        event generate $W <<ComboboxSelected>> -when mark
-    }
+  if { [$W instate readonly] } {
+    event generate $W <<ComboboxSelected>> -when mark
+  }
+  $W validate
 }
 
-proc ttk::combobox::CompleteList { W key { start -1 } } {
-    after cancel {set ttk::combobox::keyaccumulator ""}
+proc ::ttk::combobox::CompleteList { W key { start -1 } } {
+  after cancel {set ::ttk::combobox::keyaccumulator ""}
 
-    set key [EscapeKey $key]
+  set key [EscapeKey $key]
 
-    append ttk::combobox::keyaccumulator $key
-    set key $ttk::combobox::keyaccumulator
-    after 250 {set ttk::combobox::keyaccumulator ""}
+  append ::ttk::combobox::keyaccumulator $key
+  set key $::ttk::combobox::keyaccumulator
+  after 350 {set ::ttk::combobox::keyaccumulator ""}
 
-    if { $start == -1 } {
-        set start [expr { [$W curselection] + 1 }]
+  # if { $start == -1 } {
+  #   set start [expr { [$W curselection] + 1 }]
+  # }
+  for { set idx $start } { $idx < [$W size] } { incr idx } {
+    if { [string match -nocase $key* [string trim [$W get $idx] ']] } {
+      $W selection clear 0 end
+      $W selection set $idx
+      $W see $idx
+      $W activate $idx
+      return
     }
+  }
 
-    for { set idx $start } { $idx < [$W size] } { incr idx } {
-        if { [string match -nocase $key* [$W get $idx]] } {
-            $W selection clear 0 end
-            $W selection set $idx
-            $W see $idx
-            $W activate $idx
-            return
-        }
-    }
-
-    if { $start > 0 } {
-        set ttk::combobox::keyaccumulator ""
-        CompleteList $W $key 0
-    }
+  if { $start > 0 } {
+    set ::ttk::combobox::keyaccumulator ""
+    CompleteList $W $key 0
+  }
 }
 
-proc ttk::combobox::ControlKeyRelease {keycode} {
-    if {$keycode==17} {
-        set ttk::combobox::ctrlstat 0
-    }
+proc ::ttk::combobox::ControlKeyRelease {keycode} {
+  if {$keycode==17} {
+    set ::ttk::combobox::ctrlstat 0
+  }
 }
 
-bind ComboboxListbox <KeyPress>   { ttk::combobox::CompleteList %W %K }
-bind ComboboxListbox <Alt-Up>     { ttk::combobox::LBSelected %W }
 
-bind TCombobox       <KeyRelease> { ttk::combobox::CompleteEntry %W %K }
-bind TCombobox       <Up>         { ttk::combobox::PrevNext %W up }
-bind TCombobox       <Down>       { ttk::combobox::PrevNext %W down }
-bind TCombobox       <Alt-Down>   { ttk::combobox::Post %W }
+bind ComboboxListbox <KeyPress>   { ::ttk::combobox::CompleteList %W %K }
+bind ComboboxListbox <Alt-Up>     { ::ttk::combobox::LBSelected %W }
+
+bind TCombobox       <KeyRelease> { ::ttk::combobox::CompleteEntry %W %K }
+bind TCombobox       <Up>         { ::ttk::combobox::PrevNext %W up }
+bind TCombobox       <Down>       { ::ttk::combobox::PrevNext %W down }
+bind TCombobox       <Alt-Down>   { ::ttk::combobox::Post %W }
 
 # Bind to CTRL key
-bind TCombobox <Control_L> {set ttk::combobox::ctrlstat 1}
-bind TCombobox <Control_R> {set ttk::combobox::ctrlstat 1}
+bind TCombobox <Control_L> {set ::ttk::combobox::ctrlstat 1}
+bind TCombobox <Control_R> {set ::ttk::combobox::ctrlstat 1}
 bind TCombobox <Control-KeyRelease> {ttk::combobox::ControlKeyRelease %k}
